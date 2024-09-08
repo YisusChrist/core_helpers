abd4fc783f844646056b13c59de62b89be713eab
YisusChrist <agelrenorenardo@gmail.com>
2024-09-08 13:27:19 +0200

    feat(cli): add theme argument

--- core_helpers/cli.py
+++ core_helpers/cli.py
@@ -1,12 +1,41 @@
 """Command-line interface helper functions."""
 
 from argparse import ArgumentParser, _ArgumentGroup
+from enum import Enum
 
 from rich_argparse_plus import RichHelpFormatterPlus  # type: ignore
 
 
+class ArgparseColorThemes(Enum):
+    """
+    Available color themes for the argparse formatter.
+
+    Full list available on ARGPARSE_COLOR_THEMES.
+    """
+
+    DEFAULT = "default"
+    PRINCE = "prince"
+    NIGHT_PRINCE = "night_prince"
+    BLACK_AND_WHITE = "black_and_white"
+    GREY_AREA = "grey_area"
+    DARKNESS = "darkness"
+    THE_MATRIX = "the_matrix"
+    THE_LAWN = "the_lawn"
+    FOREST = "forest"
+    LILAC = "lilac"
+    MORNING_GLORY = "morning_glory"
+    THE_PINK = "the_pink"
+    DRACULA = "dracula"
+    ROSES = "roses"
+    COLD_WORLD = "cold_world"
+    MOTHER_EARTH = "mother_earth"
+
+
 def setup_parser(
-    package: str, description: str, version: str
+    package: str,
+    description: str,
+    version: str,
+    theme: ArgparseColorThemes = ArgparseColorThemes.DEFAULT,
 ) -> tuple[ArgumentParser, _ArgumentGroup]:
     """
     Create a parser with the default command-line arguments.
@@ -14,6 +43,11 @@ def setup_parser(
     Returns:
         tuple[ArgumentParser, _ArgumentGroup]: The parser and the main group.
     """
+    try:
+        RichHelpFormatterPlus.choose_theme(theme.value)  # Ensure theme is applied
+    except KeyError:
+        raise ValueError(f"Theme '{theme.value}' is not recognized by the formatter.")
+
     parser = ArgumentParser(
         description=description,  # Program description
         formatter_class=RichHelpFormatterPlus,  # Disable line wrapping
