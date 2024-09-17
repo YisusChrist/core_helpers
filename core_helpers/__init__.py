717deb528b3bb6cf0a743ce9124e8232ab7ddc9b
YisusChrist <agelrenorenardo@gmail.com>
2024-09-17 23:10:19 +0200

    fix: update __init__ imports

--- core_helpers/__init__.py
+++ core_helpers/__init__.py
@@ -1,3 +1,15 @@
-from core_helpers.cli import *
-from core_helpers.logs import *
-from core_helpers.updates import *
+from core_helpers.cli import ArgparseColorThemes, setup_parser
+from core_helpers.logs import setup_logger
+from core_helpers.updates import check_updates
+from core_helpers.utils import print_welcome
+from core_helpers.xdg_paths import get_user_path
+
+__all__: list[str] = [
+    "ArgparseColorThemes",
+    "check_updates",
+    "exit_session",
+    "get_user_path",
+    "print_welcome",
+    "setup_logger",
+    "setup_parser",
+]
